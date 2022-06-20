3618eb9598581ab79b25f2b56890432457b91f8e
--global <zackzackyack@gmail.com>
2022-06-20 13:03:20 -0400

    Removed useless lines in the generate-env script

--- prescripts/generate-env.js
+++ prescripts/generate-env.js
@@ -5,28 +5,10 @@ const SESSION_SECRET = [...Array(32)]
     .map(() => Math.floor(Math.random() * 16).toString(16))
     .join("");
 
-const defaultEnv = `# The following is given by the system admin (tech director):
-
-# =================================================================
-# Custom CMS
-STRAPI_URL=https://flow-cms.fly.dev
-STRAPI_TOKEN=
-
-# SMTP Settings
-SMTP_HOST=
-SMTP_USERNAME=
-SMTP_PASSWORD=
-SMTP_PORT=
-
-# Cloudinary
-CLOUDINARY_NAME=
-CLOUDINARY_API_KEY=
-CLOUDINARY_API_SECRET=
-# =================================================================
-
-
-# Prisma + SQLLite
+const defaultEnv = `# Prisma + SQLLite
 DATABASE_URL="file:./data.db?connection_limit=1"
+
+# Session secret. Used as encryption on cookie storage
 SESSION_SECRET=${SESSION_SECRET}
 
 # Metrics
